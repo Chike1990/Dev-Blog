3d7006e1ed607242bc4811228eee98d4f51ebf38
Chike1990 <chibuikeuzoechina@gmail.com>
2022-08-16 14:28:55 +0100

    Create custom methods for user, comment, like, and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
 
+  after_create :update_posts_counter
 
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
