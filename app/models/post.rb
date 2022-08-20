11787d6f7373f52d5cfe50dc9a6d7a38261c6af2
Chike1990 <chibuikeuzoechina@gmail.com>
2022-08-20 17:58:48 +0100

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
+  validates :title,
+            presence: { allow_blank: false, message: 'must not be blank.' },
+            length: { maximum: 250, message: 'must not exceed 250 characters' }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
