16737e5614dba0e29018eaaee7c9005f3e8867fa
Chike1990 <chibuikeuzoechina@gmail.com>
2022-08-24 00:32:38 +0100

    Fix rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
-class PostsController <ApplicationController
-    def index
-    end
+class PostsController < ApplicationController
+  def index; end
 
-    def show
-        # puts params
-    end
+  def show
+    # puts params
+  end
 end
